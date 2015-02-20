99f9a44f395cb889f32804bfdaa9c8d61dffbcb6
Andrew Finnell <andy@losingfight.com>
2015-02-19 18:35:36 -0800

    Adding an iOS target.

---
+++ ObserverSetiOS/ObserverSetiOS.h
@@ -0,0 +1,19 @@
+//
+//  ObserverSetiOS.h
+//  ObserverSetiOS
+//
+//  Created by Andrew Finnell on 2/19/15.
+//  Copyright (c) 2015 Mike Ash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObserverSetiOS.
+FOUNDATION_EXPORT double ObserverSetiOSVersionNumber;
+
+//! Project version string for ObserverSetiOS.
+FOUNDATION_EXPORT const unsigned char ObserverSetiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObserverSetiOS/PublicHeader.h>
+
+
